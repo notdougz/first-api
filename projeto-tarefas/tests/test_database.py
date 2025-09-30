b9b733c9586641d8aed9a1e480f8281925186c4c
Douglas Oliveira <denbinsk4853@gmail.com>
2025-09-30 13:17:08 -0300

    Adiciona configuração do pytest e implementa testes assíncronos

---
+++ projeto-tarefas/tests/test_database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
+# Usamos um banco de dados em memória para os testes
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
